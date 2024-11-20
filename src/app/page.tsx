1851149ec33aa75996e645b7aee91a7ce379d430
Alexander Fuerst <alexander@fuerst.one>
2024-11-20 15:26:01 +0100

    Change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@ import { getTodaysGame } from "./getTodaysGame";
 
 export default async function Home() {
   const { id, letters, possibleWords } = await getTodaysGame();
+  const todaysTitle = "buchstabiene"
+    .split("")
+    .map((letter) => (letters.includes(letter) ? "_" : letter))
+    .join("");
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-foreground text-background">
       <div className="container max-w-lg">
         <h1 className="text-xl font-bold text-center w-full mb-6">
-          BuchstaBiene
+          {todaysTitle}
         </h1>
         <GameInterface
           id={id}
