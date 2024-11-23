7650085b569ef9a8cbd8ce9c034bdebe168bf924
Alexander Fuerst <alexander@fuerst.one>
2024-11-23 15:47:25 +0100

    Add PWA Capability

---
+++ src/app/manifest.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "BuchstaBiene",
+    short_name: "BuchstaBiene",
+    description:
+      "Finde alle Wörter, die du aus den Buchstaben zusammen setzen kannst.",
+    start_url: "/spielen/heute",
+    display: "standalone",
+    background_color: "#0c0a09",
+    theme_color: "#facc15",
+    icons: [
+      {
+        src: "/icon.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
